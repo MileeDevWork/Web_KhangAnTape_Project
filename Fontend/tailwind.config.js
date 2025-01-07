c45abc17717197e3bba23ef83958556d7d1cce32
LAPTOP-6BUUF8U8\MY LE <dammaynholuyenthuyen@gmail.com>
2025-01-08 00:11:49 +0700

    Thêm thư mục Fontend và các tệp dự án

---
+++ Fontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Thêm các tệp trong thư mục src
+    "./public/index.html",             // Tệp HTML chính trong React
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        BeVietnam: ['Be Vietnam Pro', 'sans-serif'],
+        Roboto: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        green: '#156B14',
+        xanh_ma: '#F9FDE7',
+      },
+      boxShadow: {
+        'bong': '0px 4px 6px 0px #789A3D', // Định nghĩa shadow tùy chỉnh
+      },
+    },
+  },
+  plugins: [],
+}
+
